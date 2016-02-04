7e5978a5f5e80caae4e1cfecbda4c6da7a458b98
Joakim Engström <engstrom.joakim@gmail.com>
2016-02-04 21:37:25 +0100

    Added OkHttp and auth_token to a gradle file

--- mobile/src/main/java/com/joezorry/foodlist/AppModule.java
+++ mobile/src/main/java/com/joezorry/foodlist/AppModule.java
@@ -4,10 +4,16 @@ import android.content.Context;
 
 import com.joezorry.foodlist.models.OrmaDatabase;
 
+import java.io.IOException;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.JacksonConverterFactory;
 import retrofit2.Retrofit;
 
@@ -19,10 +25,26 @@ public class AppModule {
     @Provides
     @Singleton
     Retrofit provideRetrofit() {
+        OkHttpClient httpClient = new OkHttpClient();
+        httpClient.networkInterceptors().add(new Interceptor() {
+            @Override
+            public Response intercept(final Chain chain) throws IOException {
+                final Request request =
+                    chain.request()
+                         .newBuilder()
+                         .addHeader(
+                             "Authorization",
+                             BuildConfig.AUTH_TOKEN)
+                         .build();
+                return chain.proceed(request);
+            }
+        });
+
         return new Retrofit.Builder()
             .addConverterFactory(JacksonConverterFactory.create())
-            .baseUrl("BASE_URL")
-            .build();
+            .baseUrl("https://api.lifesum.com/icebox/v1/foods/en/se/Orange")
+            .client(httpClient)
+        .build();
     }
 
 
